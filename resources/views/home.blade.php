caaee0a82a0bc44fceed7336ebc385465653522b
adrianhz22 <adrianhz.prog@gmail.com>
2025-02-18 23:22:31 +0100

    Primera ruta y vista home
    Vista welcome modificada a home.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body>
+
+<h1>Pagina de inicio</h1>
+<p>Contenido de la pagina de inicio</p>
+
+<footer class="bg-gray-200 text-center">
+    <p>©2025 Todos los derechos reservados PeakPost<p>
+</footer>
+
+</body>
+</html>
