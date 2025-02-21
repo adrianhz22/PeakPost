450ed4f541ae79714e52aa8063c77cb77bb9a7de
adrianhz22 <adrianhz.prog@gmail.com>
2025-02-21 12:21:10 +0100

    Middleware auth
    Middleware para asegurarme de que no se accedan a las rutas sin autenticarse, excepto a home

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,15 @@ Route::get('/', function () {
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-Route::get('/post/{post}', [PostController::class, 'show'])->name('show');
-Route::get('/create', [PostController::class, 'create'])->name('create');
-Route::post('/store', [PostController::class, 'store'])->name('store');
-Route::get('/edit/{post}', [PostController::class, 'edit'])->name('edit');
-Route::put('/update/{post}', [PostController::class, 'update'])->name('update');
-Route::delete('/destroy/{post}', [PostController::class, 'destroy'])->name('destroy');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/post/{post}', [PostController::class, 'show'])->name('show');
+    Route::get('/create', [PostController::class, 'create'])->name('create');
+    Route::post('/store', [PostController::class, 'store'])->name('store');
+    Route::get('/edit/{post}', [PostController::class, 'edit'])->name('edit');
+    Route::put('/update/{post}', [PostController::class, 'update'])->name('update');
+    Route::delete('/destroy/{post}', [PostController::class, 'destroy'])->name('destroy');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
