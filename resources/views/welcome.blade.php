2b9ee85b865133f7efb34accf58ef3dabcfd9660
adrianhz22 <adrianhz.prog@gmail.com>
2025-02-21 15:58:00 +0100

    Vista welcome
    He creado una vista welcome para que no se pueda ver nada de la pagina a no ser que se registre el usuario.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PeakPost</title>
+</head>
+
+<body>
+
+<h1>Bienvenido a PeakPost</h1>
+
+<a href="login">Iniciar sesion</a>
+<a href="register">Registrarse</a>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [PostController::class, 'index'])->name('home');
     Route::get('/post/{post}', [PostController::class, 'show'])->name('show');
     Route::get('/create', [PostController::class, 'create'])->name('create');
     Route::post('/store', [PostController::class, 'store'])->name('store');
