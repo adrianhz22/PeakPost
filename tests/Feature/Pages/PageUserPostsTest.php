6549444dfe23634c7d33bfe9d4ddd6d472993634
adrianhz22 <adrianhz.prog@gmail.com>
2025-02-28 12:59:22 +0100

    Tests Welcome y UserPosts
    Tests para comprobar el acceso a la vista de los posts de los usuarios y tambien confirmar que aparecen las opciones de iniciar sesion o registrarse en welcome.

---
+++ tests/Feature/Pages/PageUserPostsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Models\User;
+
+it('access successful to user-posts page', function () {
+
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('user.posts'));
+
+    $response->assertStatus(200);
+});
--- tests/Feature/Pages/PageWelcomeTest.php
+++ tests/Feature/Pages/PageWelcomeTest.php
@@ -6,3 +6,13 @@ it('access successful to welcome page', function () {
 
     $response->assertStatus(200);
 });
+
+it('shows login and register buttons', function () {
+
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+    $response->assertSeeText('Iniciar Sesión');
+    $response->assertSeeText('Registrarse');
+
+});
