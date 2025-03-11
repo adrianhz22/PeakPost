2f248ffe09e2476bf43cd4f31c5cfd6bcd84a554
adrianhz22 <adrianhz.prog@gmail.com>
2025-03-11 17:43:27 +0100

    Factory para comentarios
    He añadido un factory para los comentarios con su respectivo seeder para que cada post contenga 3 comentarios y asi este mas completa la informacion de prueba.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -19,7 +20,14 @@ class DatabaseSeeder extends Seeder
             RolesSeeder::class,
         ]);
 
-        User::factory(5)->create();
-        Post::factory(15)->create();
+        $users = User::factory(5)->create();
+        $posts = Post::factory(15)->create(['user_id' => $users->random()->id]);
+
+        foreach ($posts as $post) {
+            Comment::factory(3)->create([
+                'post_id' => $post->id,
+                'user_id' => $users->random()->id,
+            ]);
+        }
     }
 }
