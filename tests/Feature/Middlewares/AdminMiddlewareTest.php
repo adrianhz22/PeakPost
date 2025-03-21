0845817b17eaa997101d92ea6fe9e6b6df5d685b
adrianhz22 <adrianhz.prog@gmail.com>
2025-03-21 12:13:30 +0100

    Tests AdminMiddleware
    AdminMiddleware cubierto comprobando que los administradores pueden acceder a todas las paginas que hay de momento en el dashboard.

---
+++ tests/Feature/Middlewares/AdminMiddlewareTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+it('non-admin users are redirected to home', function () {
+
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('admin.dashboard'));
+
+    $response->assertRedirect('/home');
+});
+
+it('admins can access dashboard', function () {
+
+    Role::create(['name' => 'admin']);
+
+    $admin = User::factory()->create();
+    $admin->assignRole('admin');
+
+    $response = $this->actingAs($admin)->get(route('admin.dashboard'));
+
+    $response->assertStatus(200);
+});
+
+it('admins can access users page', function () {
+
+    Role::create(['name' => 'admin']);
+
+    $admin = User::factory()->create();
+    $admin->assignRole('admin');
+
+    $response = $this->actingAs($admin)->get(route('admin.users'));
+
+    $response->assertStatus(200);
+});
+
+it('admins can access posts page', function () {
+
+    Role::create(['name' => 'admin']);
+
+    $admin = User::factory()->create();
+    $admin->assignRole('admin');
+
+    $response = $this->actingAs($admin)->get(route('admin.posts'));
+
+    $response->assertStatus(200);
+});
