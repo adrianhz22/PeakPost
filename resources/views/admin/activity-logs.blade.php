9e675e7c28d8f644bf003af49e5edb181e5e888c
adrianhz22 <adrianhz.prog@gmail.com>
2025-03-26 23:57:31 +0100

    Tailwind vista del historial
    He añadido unas plantillas de tailwind para que se diferencie mejor cada acción, además un if para asignar un color a cada registro del historial.

--- resources/views/admin/activity-logs.blade.php
+++ resources/views/admin/activity-logs.blade.php
@@ -2,15 +2,31 @@
     <h1 class="text-3xl font-bold mb-6 text-center">Historial de actividad</h1>
     <div class="container mx-auto px-4 py-6">
         <div class="space-y-4">
-            @foreach($logs as $log)
-                <div>
-                    <div>
-                        <p>{{ ($log->action) }}</p>
-                        <p>{{ $log->description }}</p>
-                        <p>{{ $log->created_at->format('d/m/Y H:i') }}</p>
+
+            @foreach ($logs as $log)
+                <div class="flex justify-between items-center p-3 mb-3 text-base font-medium border rounded-lg shadow-md
+                    @if (in_array($log->action, ['Usuario eliminado', 'Comentario eliminado', 'Post eliminado', 'Post rechazado']))
+                        text-red-700 border-red-300 bg-red-100
+                    @elseif ($log->action == 'Post actualizado')
+                        text-yellow-700 border-yellow-300 bg-yellow-100
+                    @elseif (in_array($log->action, ['Usuario registrado', 'Nuevo comentario', 'Nuevo post creado', 'Post aprobado']))
+                        text-green-700 border-green-300 bg-green-100
+                    @else
+                        text-gray-700 border-gray-300 bg-gray-100
+                    @endif" role="alert">
+                    <div class="flex items-center">
+                        <svg class="shrink-0 inline w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        <span class="font-semibold">{{ $log->action }}</span> : {{ $log->description }}
+                    </div>
+
+                    <div class="text-gray-600 text-xs whitespace-nowrap">
+                        {{ $log->created_at->format('d/m/Y H:i') }}
                     </div>
                 </div>
             @endforeach
+
         </div>
     </div>
 </x-layouts.admin-layout>
