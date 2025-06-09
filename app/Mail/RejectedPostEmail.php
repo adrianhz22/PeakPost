0c47292eba73fd3dde7abc057aa0378bd4644dd7
adrianhz22 <adrianhz.prog@gmail.com>
2025-06-09 22:31:32 +0200

    .env.example configuración final nombre BBDD
    Configuración final del .env.example

--- app/Mail/RejectedPostEmail.php
+++ app/Mail/RejectedPostEmail.php
@@ -39,7 +39,7 @@ class RejectedPostEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.approved-post',
+            view: 'emails.rejected-post',
         );
     }
 
